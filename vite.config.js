330be8d621f6d04401d0ce0cc48baf78526c7bd9
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-11-05 04:38:45 -0300

    Ajustes vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Detecta si estamos haciendo build para GitHub Pages
-const isGhPages = import.meta.env.VITE_DEPLOY_ENV === "GH_PAGES";
+const isGhPages = (import.meta.env.VITE_DEPLOY_ENV ?? "") === "GH_PAGES";
 
 export default defineConfig({
   plugins: [react()],
-  base: isGhPages ? "/palacio_san_juan/" : "/", // ✅ base dinámica
+  base: isGhPages ? "/palacio_san_juan/" : "/",
   build: {
     outDir: "dist",
     sourcemap: false,
