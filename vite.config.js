f64527a83abfb617aaff206c6e04444965348534
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-11-05 04:41:08 -0300

    Ajustes vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Detecta si estamos haciendo build para GitHub Pages
-const isGhPages = (import.meta.env.VITE_DEPLOY_ENV ?? "") === "GH_PAGES";
+const isGhPages = process.env.VITE_DEPLOY_ENV === "GH_PAGES";
 
 export default defineConfig({
   plugins: [react()],
