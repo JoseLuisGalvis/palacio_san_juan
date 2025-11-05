21de716be1e22a55d610e9b89ab199f8a489c6b4
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-11-05 04:36:01 -0300

    Ajustes vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-/**
- * Detecta si estamos haciendo deploy en GitHub Pages
- * Esto funciona porque tu script de deploy es "npm run deploy"
- */
-const isGhPages = process.env.npm_lifecycle_event === "deploy";
+// Detecta si estamos haciendo build para GitHub Pages
+const isGhPages = import.meta.env.VITE_DEPLOY_ENV === "GH_PAGES";
 
 export default defineConfig({
   plugins: [react()],
-  base: isGhPages ? "/palacio_san_juan/" : "/", // ✅ dinámico según entorno
+  base: isGhPages ? "/palacio_san_juan/" : "/", // ✅ base dinámica
   build: {
-    outDir: "dist", // opcional, por defecto es "dist"
-    sourcemap: false, // opcional: evita map files innecesarios en producción
+    outDir: "dist",
+    sourcemap: false,
   },
 });
