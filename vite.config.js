c98001985eb77d11d0d4c5148762b21bc037a5d7
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-11-17 19:20:42 -0300

    Ajustes ViteConfig

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,17 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
+    chunkSizeWarningLimit: 1000, // Aumenta el límite a 1000 KB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separa las librerías principales
+          "react-vendor": ["react", "react-dom"],
+          "bootstrap-vendor": ["bootstrap"],
+          "lucide-vendor": ["lucide-react"],
+          "aos-vendor": ["aos"],
+        },
+      },
+    },
   },
 });
