fed948e2ea4dbb87ecd26cd2df91625111523ce8
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-11-29 14:39:40 -0300

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ Chequea la variable correcta que defines en package.json
-const isGhPages = process.env.VITE_DEPLOY_ENV === "GH_PAGES";
-
 export default defineConfig({
   plugins: [react()],
-  base: isGhPages ? "/palacio_san_juan/" : "/",
+  base: "./",
   build: {
     outDir: "dist",
     sourcemap: false,
